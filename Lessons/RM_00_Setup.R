947d487717da21a3ffa33eef240ea2feb6c51c3b
alex-strobel <87649655+alex-strobel@users.noreply.github.com>
2021-07-29 07:49:20 +0200

    Created subfolders

---
+++ Lessons/RM_00_Setup.R
@@ -0,0 +1,16 @@
+# Setting up RStudio for RMarkdown 
+
+# Rendering RMarkdown documents requires updated versions of the following packages:
+install.packages("base64enc")
+install.packages("bitops")
+install.packages("caTools")
+install.packages("highr")
+install.packages("htmltools")
+install.packages("jsonlite")
+install.packages("knitr")
+install.packages("markdown")
+install.packages("yaml")
+
+# You may also get an error message even if you have already installed some 
+# LaTeX bundle. In this case or if you don't have LaTex try the following:
+tinytex::install_tinytex()
