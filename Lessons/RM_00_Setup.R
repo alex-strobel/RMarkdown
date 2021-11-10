d76efaacc0b63bd355dbf8623adb6c86944053a1
Alex Strobel <87649655+alex-strobel@users.noreply.github.com>
2021-11-10 11:28:50 +0100

    Update RM_00_Setup.R

--- Lessons/RM_00_Setup.R
+++ Lessons/RM_00_Setup.R
@@ -10,6 +10,7 @@ install.packages("jsonlite")
 install.packages("knitr")
 install.packages("markdown")
 install.packages("yaml")
+install.packages("tinytex")
 
 # You may also get an error message even if you have already installed some 
 # LaTeX bundle. In this case or if you don't have LaTeX try the following:
