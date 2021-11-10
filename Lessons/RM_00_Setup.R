9909d53c4dd7c9309d32f69b3b57c7831fac7007
Alex Strobel <87649655+alex-strobel@users.noreply.github.com>
2021-11-10 11:58:26 +0100

    Update RM_00_Setup.R

--- Lessons/RM_00_Setup.R
+++ Lessons/RM_00_Setup.R
@@ -1,19 +1,27 @@
-# Setting up RStudio for RMarkdown 
+# SETTING UP RSTUDIO FOR RMARKDWON AND THIS COURSE ----------------------------
 
-# Rendering RMarkdown documents requires updated versions of the following packages:
-install.packages("base64enc")
-install.packages("bitops")
-install.packages("caTools")
-install.packages("highr")
-install.packages("htmltools")
-install.packages("jsonlite")
-install.packages("knitr")
-install.packages("markdown")
-install.packages("rmarkdown")
-install.packages("tinytex")
-install.packages("yaml")
-
+# Rendering the RMarkdown documents in this course requires updated versions of
+# the following packages (and their dependencies):
+install.packages("base64enc")   # Tools for base64 encoding
+install.packages("bitops")      # Bitwise Operations
+install.packages("caTools")     # Moving Window Statistics, GIF, ROC AUC, etc.
+install.packages("devtools")    # Tools to Make Developing R Packages Easier    
+install.packages("highr")       # Syntax Highlighting for R Source Code
+install.packages("htmltools")   # Tools for HTML
+install.packages("jsonlite")    # JSON Parser and Generator for R
+install.packages("knitr")       # Package for Dynamic Report Generation in R
+install.packages("markdown")    # Render Markdown with the C Library 'Sundown'
+install.packages("psych")       # Procedures for Psychological Research
+install.packages("rmarkdown")   # Dynamic Documents for R
+install.packages("robustbase")  # Basic Robust Statistics
+install.packages("shape")       # Functions for Plotting Shapes & Colors
+install.packages("tinytex")     # Install/Maintain TeXLive and Compile LaTeX 
+install.packages("yaml")        # Methods to Convert R Data to YAML and Back       
 
 # You may also get an error message even if you have already installed some 
 # LaTeX bundle. In this case or if you don't have LaTeX try the following:
 tinytex::install_tinytex()
+
+# You also need the papaja package that (as of 2021-11-10) is not yet on CRAN
+devtools::install_github("crsh/papaja")
+devtools::install_github("crsh/papaja@devel")
